d8bb2004440f487ce87ece7346f0009c3144631c
Pretics <ncludns@gmail.com>
2025-03-25 01:58:10 +0900

    train.py, test.py의 전반적인 구조 변경, 이에 맞춰 .ipynb 파일도 import module 형식으로 코드를 간소화함

--- project/for_test.py
+++ project/for_test.py
@@ -1,21 +1,28 @@
-import yaml
-from dotmap import DotMap
 from os import path
+from dataclasses import dataclass
+from typing import Union
+from tqdm import tqdm
+from dotmap import DotMap
+
 import numpy as np
+import yaml
+
 import torch
+from torch import Tensor
 from torch.utils.data import DataLoader
+
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning import Trainer
+
 from models.lstur import LSTUR
 from models.nrms import NRMS
 from models.naml import NAML
 from models.naml_simple import NAML_Simple
 from models.sentirec import SENTIREC
 from models.robust_sentirec import ROBUST_SENTIREC
+
 from data.dataset import BaseDataset
-from tqdm import tqdm
-from dataclasses import dataclass
 
 @dataclass
 class TestArgs():
@@ -29,99 +36,108 @@ def load_model_config(args: TestArgs):
 
     assert(config.name in ["lstur", "nrms", "naml", "naml_simple", "sentirec", "robust_sentirec"])
     pl.seed_everything(1234)
-
     return config
 
-def create_dataloader(config, preprocess_data_dir):
+def create_logger(config: DotMap):
+    logger = TensorBoardLogger(**config.logger)
+    return logger
+
+def create_dataloader(config: DotMap):
     test_dataset = BaseDataset(
-        path.join(preprocess_data_dir + config.test_behavior),
-        path.join(preprocess_data_dir + config.test_news), 
-        config)
+        path.join(config.preprocess_data_dir, config.test_behavior),
+        path.join(config.preprocess_data_dir, config.test_news), 
+        config
+    )
     test_loader = DataLoader(
         test_dataset,
-        **config.test_dataloader)
-    
-    return test_loader
+        **config.test_dataloader
+    )
+    return test_dataset, test_loader
 
-def load_embedding_weights(config, preprocess_data_dir):
+def load_embedding_weights(config: DotMap):
     # load embedding pre-trained embedding weights
     embedding_weights=[]
-    with open(path.join(preprocess_data_dir + config.embedding_weights), 'r') as file: 
+    with open(path.join(config.preprocess_data_dir, config.embedding_weights), 'r') as file: 
         lines = file.readlines()
         for line in tqdm(lines):
             weights = [float(w) for w in line.split(" ")]
             embedding_weights.append(weights)
     pretrained_word_embedding = torch.from_numpy(
         np.array(embedding_weights, dtype=np.float32)
-        )
-    
+    )
     return pretrained_word_embedding
 
-def load_model_from_checkpoint(args: TestArgs, config, pretrained_word_embedding):
+def load_model_from_checkpoint(args: TestArgs, config: DotMap, pretrained_word_embedding: Tensor):
     if config.name == "lstur":
         model = LSTUR.load_from_checkpoint(
             args.ckpt, 
             config=config, 
-            pretrained_word_embedding=pretrained_word_embedding)
+            pretrained_word_embedding=pretrained_word_embedding
+        )
     elif config.name == "nrms":
         model = NRMS.load_from_checkpoint(
             args.ckpt, 
             config=config, 
-            pretrained_word_embedding=pretrained_word_embedding)
+            pretrained_word_embedding=pretrained_word_embedding
+        )
     elif config.name == "naml":
         model = NAML.load_from_checkpoint(
             args.ckpt, 
             config=config, 
-            pretrained_word_embedding=pretrained_word_embedding)
+            pretrained_word_embedding=pretrained_word_embedding
+        )
     elif config.name == "naml_simple":
         model = NAML_Simple.load_from_checkpoint(
             args.ckpt, 
             config=config, 
-            pretrained_word_embedding=pretrained_word_embedding)
+            pretrained_word_embedding=pretrained_word_embedding
+        )
     elif config.name == "sentirec":
         model = SENTIREC.load_from_checkpoint(
             args.ckpt, 
             config=config, 
-            pretrained_word_embedding=pretrained_word_embedding)
+            pretrained_word_embedding=pretrained_word_embedding
+        )
     elif config.name == "robust_sentirec":
         model = ROBUST_SENTIREC.load_from_checkpoint(
             args.ckpt, 
             config=config, 
-            pretrained_word_embedding=pretrained_word_embedding)
-        
+            pretrained_word_embedding=pretrained_word_embedding
+        )
     return model
 
-def cli_main(args: TestArgs):
-    config = load_model_config(args)
-
-    # ------------
-    # logging
-    # ------------
-    logger = TensorBoardLogger(
-        **config.logger
-    )
-
-    # ------------
-    # data
-    # ------------
-    preprocess_data_dir = f"{config.preprocess_data_path}/{config.dataset_size}/"
-    test_loader = create_dataloader(config, preprocess_data_dir)
-   
-    # ------------
-    # init model
-    # ------------
-    pretrained_word_embedding = load_embedding_weights(config, preprocess_data_dir)
-    model = load_model_from_checkpoint(args, config, pretrained_word_embedding)
-
-    # ------------
-    # Test
-    # ------------
+def create_trainer(config: DotMap, logger: TensorBoardLogger):
     trainer = Trainer(
         **config.trainer,
         logger=logger
     )
+    return trainer
 
-    trainer.test(
+def start_test(
+        trainer: Trainer,
+        model: Union[LSTUR, NRMS, NAML, NAML_Simple, SENTIREC, ROBUST_SENTIREC],
+        test_loader: DataLoader
+    ) -> list[dict[str, float]]:
+    test_result = trainer.test(
         model=model, 
         dataloaders=test_loader
     )
+    return test_result
+
+def cli_main(args: TestArgs):
+    # config
+    config = load_model_config(args)
+
+    # logging
+    logger = create_logger(config)
+
+    # load data
+    test_dataset, test_loader = create_dataloader(config)
+    pretrained_word_embedding = load_embedding_weights(config)
+   
+    # init model
+    model = load_model_from_checkpoint(args, config, pretrained_word_embedding)
+
+    # Test
+    trainer = create_trainer(config, logger)
+    test_result = start_test(trainer, model, test_loader)
--- project/train.py
+++ project/train.py
@@ -1,29 +1,7 @@
 import argparse
-import yaml
-from dotmap import DotMap
-from os import path
-import numpy as np
-import torch
-from torch.utils.data import DataLoader
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning import Trainer
-from pytorch_lightning.strategies import DDPStrategy
-from models.lstur import LSTUR
-from models.nrms import NRMS
-from models.naml import NAML
-from models.naml_simple import NAML_Simple
-from models.sentirec import SENTIREC
-from models.robust_sentirec import ROBUST_SENTIREC
-from data.dataset import BaseDataset
-from tqdm import tqdm
+import for_train
 
-
-def cli_main():
-    # ------------
-    # args
-    # ------------
+def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--config',
@@ -38,108 +16,9 @@ def cli_main():
         help='resume training from ckpt',
         required=False)
     args = parser.parse_args()
-    
-    with open(args.config, 'r') as ymlfile:
-        config = yaml.load(ymlfile, Loader=yaml.FullLoader)
-        config = DotMap(config)
-
-    print(config)
-
-    assert(config.name in ["lstur", "nrms", "naml", "naml_simple", "sentirec", "robust_sentirec"])
-
-    pl.seed_everything(1234)
-    
-    # ------------
-    # init callbacks & logging
-    # ------------
-    checkpoint_callback = ModelCheckpoint(
-        **config.checkpoint
-    )
-    logger = TensorBoardLogger(
-        **config.logger
-    )
-
-    # ------------
-    # data
-    # ------------
-
-    preprocess_path = f"{config.preprocess_data_path}/{config.dataset_size}/"
-
-    train_dataset = BaseDataset(
-        path.join(preprocess_path+config.train_behavior),
-        path.join(preprocess_path+config.train_news), 
-        config)
-    val_dataset = BaseDataset(
-        path.join(preprocess_path+config.val_behavior),
-        path.join(preprocess_path+config.train_news), 
-        config) 
-    train_loader = DataLoader(
-        train_dataset,
-        **config.train_dataloader)
-    val_loader = DataLoader(
-        val_dataset,
-        **config.val_dataloader)
-   
-    # ------------
-    # init model
-    # ------------
-    # load embedding pre-trained embedding weights
-    embedding_weights=[]
-    with open(path.join(preprocess_path+config.embedding_weights), 'r') as file: 
-        lines = file.readlines()
-        for line in tqdm(lines):
-            weights = [float(w) for w in line.split(" ")]
-            embedding_weights.append(weights)
-    pretrained_word_embedding = torch.from_numpy(
-        np.array(embedding_weights, dtype=np.float32)
-        )
-
-    if config.name == "lstur":
-        model = LSTUR(config, pretrained_word_embedding)
-    elif config.name == "nrms":
-        model = NRMS(config, pretrained_word_embedding)
-    elif config.name == "naml":
-        model = NAML(config, pretrained_word_embedding)
-    elif config.name == "naml_simple":
-        model = NAML_Simple(config, pretrained_word_embedding)
-    elif config.name == "sentirec":
-        model = SENTIREC(config, pretrained_word_embedding)
-    elif config.name == "robust_sentirec":
-        model = ROBUST_SENTIREC(config, pretrained_word_embedding)
-    # elif:
-        # UPCOMING MODELS
-
-    # ------------
-    # training
-    # ------------
-    early_stop_callback = EarlyStopping(
-       **config.early_stop
-    )
-    if args.resume is not None:
-        model = model.load_from_checkpoint(
-            args.resume, 
-            config=config, 
-            pretrained_word_embedding=pretrained_word_embedding)
-        trainer = Trainer(
-            **config.trainer,
-            callbacks=[early_stop_callback, checkpoint_callback],
-            logger=logger,
-            #strategy=DDPStrategy(process_group_backend="gloo"),
-            #plugins=DDPPlugin(find_unused_parameters=config.find_unused_parameters), 
-            resume_from_checkpoint=args.resume
-        )
-    else:
-        trainer = Trainer(
-            **config.trainer,
-            callbacks=[early_stop_callback, checkpoint_callback],
-            logger=logger,
-            #strategy=DDPStrategy(process_group_backend="gloo")
-            #plugins=DDPPlugin(find_unused_parameters=config.find_unused_parameters)
-        )
-    trainer.fit(
-        model=model, 
-        train_dataloaders=train_loader, 
-        val_dataloaders=val_loader)
+    args = for_train.TrainArgs(**vars(args))
+    return args
 
 if __name__ == '__main__':
-    cli_main()
+    args = get_args()
+    for_train.cli_main(args)
